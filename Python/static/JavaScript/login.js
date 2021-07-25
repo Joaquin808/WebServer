ec65b776b083ee6242c58516ebeb26e1fea50cd6
Kin <bellj0521@gmail.com>
2021-07-25 16:34:22 +0100

    File directory changes

--- Python/static/JavaScript/login.js
+++ Python/static/JavaScript/login.js
@@ -5,7 +5,7 @@ function login(){
 	xttp.onreadystatechange = function() {
 		console.log(this.readyState);
 		if (this.status == 200 && this.readyState == 4){
-			window.location.href = "/home/pi/Desktop/WebServer/WebFiles/MainPage.html";
+			window.location.href = "/home/pi/Desktop/WebServer/Python/templates/main.html";
 		}
 	}
 	
