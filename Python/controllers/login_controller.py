ecc23099a11154a0ce0542a0f92724ba574cbeac
Kin <bellj0521@gmail.com>
2021-07-26 11:32:57 +0100

    Small changes

--- Python/controllers/login_controller.py
+++ Python/controllers/login_controller.py
@@ -8,9 +8,9 @@ def route(app):
     @app.route("/login", methods=["GET"])
     def login():
        #  start_response('201 OK', [('Content-Type', 'application/json',
-        ('Access-Control-Allow-Headers','authorization'),
-        ('Access-Control-Allow-Methods','HEAD, GET, POST, PUT, PATCH, DELETE'),
-        ('Access-Control-Allow-Origin','*'),
-        ('Access-Control-Max-Age','600'))])
+       #  ('Access-Control-Allow-Headers','authorization'),
+       # ('Access-Control-Allow-Methods','HEAD, GET, POST, PUT, PATCH, DELETE'),
+       #  ('Access-Control-Allow-Origin','*'),
+       #  ('Access-Control-Max-Age','600'))])
 
         return jsonify(LoginService.check_login(request.json["username"], request.json["password"])), 200
