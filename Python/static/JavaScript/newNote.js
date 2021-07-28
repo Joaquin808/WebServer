3e66184da878c672375142bf3188e72b80d8796a
Kin <bellj0521@gmail.com>
2021-07-28 16:31:27 +0100

    Added button to create new notes

--- Python/static/JavaScript/newNote.js
+++ Python/static/JavaScript/newNote.js
@@ -1,19 +1,23 @@
-let noteName = document.getElementById("noteName").value;
-let noteContent = document.getElementById("noteContent").value;
+document.getElementById("sendNote").addEventListener("click", sendNote);
 
-let xttp = new XMLHttpRequest();
+function sendNote(){	
+	let noteName = document.getElementById("noteName").value;
+	let noteContent = document.getElementById("noteContent").value;
 
-xttp.onreadystatechange = function() {
-	console.log(this.readyState);
-	if (this.readyState == 4 && this.status == 200){
-		console.log(this.responseText);
+	let xttp = new XMLHttpRequest();
+
+	xttp.onreadystatechange = function() {
+		console.log(this.readyState);
+		if (this.readyState == 4 && this.status == 200){
+			console.log(this.responseText);
+		}
 	}
+
+	let url = "http://10.0.0.141:5000/note";
+	xttp.open("POST", url, true);
+	xttp.setRequestHeader("Content-Type", "application/json");
+
+	let noteObject = {"url": "", "noteName": noteName, "noteContent": noteContent};
+	console.log(noteObject);
+	xttp.send(noteObject);
 }
-
-let url = "http://10.0.0.141:5000/note";
-xttp.open("POST", url, true);
-xttp.setRequestHeader("Content-Type", "application/json");
-
-let noteObject = {"url": "", "noteName": noteName, "noteContent": noteContent};
-console.log(noteObject);
-xttp.send(noteObject);
