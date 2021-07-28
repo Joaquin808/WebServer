aae317be0d922633c58f1f8acf70191c179f9641
Kin <bellj0521@gmail.com>
2021-07-28 04:43:29 +0100

    Create note webpage

---
+++ Python/static/JavaScript/newNote.js
@@ -0,0 +1,19 @@
+let noteName = document.getElementById("noteName").value;
+let noteContent = document.getElementById("noteContent").value;
+
+let xttp = new XMLHttpRequest();
+
+xttp.onreadystatechange = function() {
+	console.log(this.readyState);
+	if (this.readyState == 4 && this.status == 200){
+		console.log(this.responseText);
+	}
+}
+
+let url = "http://10.0.0.141:5000/note";
+xttp.open("POST", url, true);
+xttp.setRequestHeader("Content-Type", "application/json");
+
+let noteObject = {"url": "", "noteName": noteName, "noteContent": noteContent};
+console.log(noteObject);
+xttp.send(noteObject);
