d9ea8e6ba2d6315d2541ea8031919bc724e364bb
Kin <bellj0521@gmail.com>
2021-07-29 20:35:04 +0100

    Change to display note contents on main page

--- Python/static/JavaScript/main.js
+++ Python/static/JavaScript/main.js
@@ -1,10 +1,11 @@
 let xttp = new XMLHttpRequest();
 
+let allNotes = {};
 xttp.onreadystatechange = function() {
 	console.log(this.readyState);
 	if (this.readyState == 4 && this.status == 200){
 		console.log(this.responseText);
-		let notes = JSON.parse(this.responseText);
+		allNotes = JSON.parse(this.responseText);
 		let noteView = document.getElementById("noteView");
 		let keys = Object.keys(notes);
 		for (key in keys){
@@ -20,6 +21,11 @@ let url = "http://10.0.0.141:5000/notes";
 xttp.open("GET", url, true);
 
 xttp.send();
+
+// This function will display all of the contents within that specified note
+function viewNote(noteName){
+	document.getElementById("noteDisplay").innerHTML = allNote[noteName];
+}
 
 document.getElementById("createButton").addEventListener("click", NewNote);
 function NewNote(){
