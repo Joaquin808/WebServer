e1c7bf84486722138a524a8154c3b3662d9543c7
Kin <bellj0521@gmail.com>
2021-08-06 04:17:12 +0100

    Images files updated

--- Python/static/JavaScript/image.js
+++ Python/static/JavaScript/image.js
@@ -1,1 +1,12 @@
-// For image upload
+window.addEventListener("upload", function(){
+	document.querySelector('input[type="file"]').addEventListener("change", function() {
+		if (this.files && this.files[0]) {
+			var img = document.querySelector('img');
+			img.onload = () => {
+				URL.revokeObjectURL(img.src); // no longer needed, free memory
+			}
+
+			img.src = URLcreateObjectURL(this.files[0]); // set src to blob url
+		}
+	});
+});
